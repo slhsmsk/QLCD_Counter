10781eacfa11f20ec78f1575eb097004b8a19f86
slhsmsk <slh.smsk@hotmail.com>
2020-08-19 16:28:04 +0300

    QLCD_counter Projesi ilk yükleme

---
+++ LCDNumber.h
@@ -0,0 +1,73 @@
+#ifndef MYWINDOW_H
+#define MYWINDOW_H
+#include<QMainWindow>
+#include<QMessageBox>
+#include<QKeyEvent>
+#include<QWidget>
+#include<QtGui>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QLCDNumber>
+
+class LCDNumber:public QLCDNumber
+{
+    Q_OBJECT
+
+public:
+    QTimer* zamanlayici;
+    QTime* zamansaat;
+    QPushButton* buton1;
+    QPushButton* buton2;
+
+public:
+    LCDNumber(QVBoxLayout *& verticalLayout,QMainWindow *window,int minutes,int seconds)
+    {
+        zamanlayici = new QTimer();
+        zamansaat =   new QTime(0,minutes,seconds,0);
+
+      QPushButton *buton1 = new QPushButton(QString::fromUtf8("Başla"));
+      QPushButton *buton2 = new QPushButton(("Dur"));
+
+        buton1->setAutoExclusive(true);//bu özelliği kazandır.
+        buton2->setAutoExclusive(true);
+
+        buton1->setCheckable(true);//kazandırdığıö özelliği kulllanabilmek için atıyorum.
+        buton2->setCheckable(true);
+
+        verticalLayout->addWidget(this);
+        verticalLayout->addWidget(buton1);
+        verticalLayout->addWidget(buton2);
+
+        this->display(zamansaat->toString(QString("mm:ss.zzz")));
+
+        QObject::connect(zamanlayici,SIGNAL(timeout()),this,SLOT(setDisplay));
+        QObject::connect(buton1,SIGNAL(pressed()),this,SLOT(start()));
+        QObject::connect(buton2,SIGNAL(pressed()),this,SLOT(stop()));
+
+    }
+    ~LCDNumber(){};
+
+    public slots:
+
+    void setDisplay(){
+        this->zamansaat->setHMS(0,this->zamansaat->addMSecs(-1).minute(),
+                                  this->zamansaat->addMSecs(-1).second(),
+                                  this->zamansaat->addMSecs(-1).msec());
+
+        this->display(this->zamansaat->toString(QString("mm::ss,zzz")));
+
+
+    }
+
+    void start()
+    {
+        this->zamanlayici->start();
+    }
+
+    void stop()
+    {
+        this->zamanlayici->stop();
+    }
+};
+
+#endif // MYWINDOW_H
---
+++ main.cpp
@@ -0,0 +1,49 @@
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QVBoxLayout>
+#include <QGroupBox>
+#include <QListWidget>
+#include <QSpinBox>
+#include <QString>
+#include <QDebug>
+#include <QFile>
+#include <QDir>
+#include <QLabel>
+#include <QTextEdit>
+#include<LCDNumber.h>
+
+int main(int argc, char *argv[]){
+
+
+    //QApplication app(argc, argv);
+    QApplication uygulama(argc,argv);
+
+    QMainWindow *window = new QMainWindow();
+
+    window->setWindowTitle(QString::fromUtf8("QLCD Number Sayac"));
+    window->resize(800,300);
+
+    QWidget *centerWd = new QWidget();
+
+    QVBoxLayout *yrlsm =  new QVBoxLayout();
+    centerWd->setLayout(yrlsm);
+    
+      LCDNumber *sayi = new LCDNumber(yrlsm,window,1,0);
+
+       sayi->setFixedSize(800,280);
+       sayi->setDigitCount(9);
+
+       window->setCentralWidget(centerWd);//ortalamak için yazılan
+       window->show();
+
+       sayi->zamanlayici->setInterval(1);
+       sayi->zamanlayici->start();
+
+
+
+    return uygulama.exec();
+}
+
